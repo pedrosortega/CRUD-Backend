c2bc7dfa27ffa18b079f66caca305f19d3d5eb3a
Dagostocsc <Deborahagosto19@gmail.com>
2025-07-01 18:46:12 -0400

    Created Tables for Students -Deb

--- database/seed.js
+++ database/seed.js
@@ -1,19 +1,72 @@
 const db = require("./db");
-const { Duck } = require("./index");
+const { DataTypes } = require("sequelize");
 
-const seed = async () => {
+const Student = db.define('student',
+  {
+    user_id:
+    {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    firstName:
+    {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: { notEmpty: true}
+    },
+    lastName:
+    {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {notEmpty: true }
+    },
+    imageURL:
+    {
+      type: DataTypes.STRING,
+      defaultValue: "https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg",
+      allowNull: true
+    },
+    email: 
+    {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {notEmpty: true}
+    },
+    gpa:
+    {
+      type: DataTypes.FLOAT(0.0),
+      allowNull: false,
+    }
+  } 
+)
+  const seed = async () => {
   db.logging = false;
   await db.sync({ force: true }); // Drop and recreate tables
-  const ducks = await Duck.bulkCreate([
-    { name: "James Pond" },
-    { name: "Quakie Chan" },
-    { name: "Goose" },
+  const students = await Student.bulkCreate([
+    { firstName: "Florencio", lastName: "Rendon", email: "flo@gmail.com", gpa: 3.9},
+    { firstName: "Deb", lastName: "Agosto", email: "deb@gmail.com", gpa: 3.9 },
+    { firstName: "Pedro", lastName: "Ortega", email: "pedro@gmail.com", gpa: 3.9 },
+    { firstName: "Mark", lastName: "Fa", email: "mark@gmail.com", gpa: 3.9}
   ]);
-
-  console.log(`🦆 Created ${ducks.length} ducks`);
 
   console.log("🌱 Seeded the database");
   db.close();
 };
 
 seed();
+
+// Table students {
+//   id INTEGER [primary key]
+//   firstName STRING
+//   lastName STRING
+//   email STRING
+//   imageUrl STRING
+//   gpa FLOAT
+// }
+
+// Table campuses {
+//   id INTEGER [primary key]
+//   name STRING
+// }
