5bb47659544b04f5f803794936ab8e43fa4b375d
rend1027 <rend1027@yahoo.com>
2025-07-01 19:31:24 -0400

    created api routes

--- api/index.js
+++ api/index.js
@@ -1,7 +1,11 @@
-const express = require("express");
-const router = express.Router();
-const ducksRouter = require("./ducks");
+const express = require("express");      // import express as a express --loads express library
+const router = express.Router();        // creates a modular router instance ---reusable
+const studentsRouter = require("./students");   // an instance of a router 
+const campusesRouter = require("./campuses")
 
-router.use("/ducks", ducksRouter);
+                  //subrouter
+//This mounts the studentsRouter under the '/students path'.
+router.use("/students", studentsRouter); // All routes inside studentsRouter will be available under /students----> router.use(path, routerModule) 
+router.use("/campuses", campusesRouter)  // All routes inside studentsRouter will be available under /campuses----> router.use(path, routerModule)
 
 module.exports = router;
--- api/students.js
+++ api/students.js
@@ -2,8 +2,13 @@ const express = require("express");
 const router = express.Router();
 const { Duck } = require("../database");
 
-// GET all ducks
+// GET all students
 router.get("/", async (req, res) => {
+  try {
+    
+  } catch(error) {
+    console.log("failed to fetch all users", error)
+  }
   res.sendStatus(501);
 });
 
