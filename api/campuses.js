f1adcfb0c3cb0c442211639aab14d3316abd4ec8
rend1027 <rend1027@yahoo.com>
2025-07-02 12:58:31 -0400

    delete student api

--- api/campuses.js
+++ api/campuses.js
@@ -1,12 +1,18 @@
-// const express = require('express');
-// const router = express.Router();
-// const {Campuses} = require('../database');
+const express = require('express');
+const router = express.Router();
+const {Campuses} = require('../database');
 
-// router.get('/', (request, response) => {
-//     try{
-        
-//     } catch(error) {
-//         console.log("Failed to fetch all ")
-//         response.send(500).json({error: ""})
-//     }
-// })
+
+// Get all campuses
+router.get('/', async (request, response) => {
+    try{
+        const campuses = await Campuses.findAll();
+        response.status(200).send(campuses);
+    } catch(error) {
+        console.log("Failed to fetch all ")
+        response.status(500).json({error: "Failed to fetch students"});
+    }
+});
+
+//Get campuses by id
+router.get("/:id")
--- api/students.js
+++ api/students.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();   // // creates a modular router instance ---reusable
 const Student  = require("../database/seed");
-const { request } = require("../app");
+const { request, response } = require("../app");
 
 // GET all students
-// res.status(200).send(students)
+
 router.get("/", async (req, res) => {
   try {
     const students = await Student.findAll();
-    res.send(students);
+    res.status(200).send(students);
   } catch(error) {
     res.status(500).json({error: "Failed to fetch tasks"});
   }
@@ -21,7 +21,7 @@ router.get("/:id", async (req, res) => {
     if (!student) {
       return res.status(404).json({error: "Student not found"});
     }
-    res.send(student);
+    res.status.apply(200).send(student);
   } catch(error) {
     res.status(500).json({ error: "Failed to fetch task"});
   }
@@ -30,18 +30,48 @@ router.get("/:id", async (req, res) => {
 //Patch a task by id
 router.patch("/:id", async (request, response) => {
   try {
-    const student = await Student.findByPk(req.params.id);
+    const student = await Student.findByPk(request.params.id);
     if (!student) {
-      return res.status(404).json({error: "Student not found"})
+      return response.status(404).json({error: "Student not found"})
     }
-    const updatedStudent = await student.update(req.body);
-    res.send(updatedStudent);
+    const updatedStudent = await student.update(request.body);
+    respond.status(200).send(updatedStudent);
   } catch (error) {
-    res.status(500).json({ error: "Failed to fetch task"})
+    response.status(500).json({ error: "Failed to fetch task"})
   }
 })
 
 
-//Delete a task by id
+//Delete a student by id
+router.delete("/:id", async (request, response) => {
+  try {
+    const student = await Student.findByPk(request.params.id);
+    if (!student) {
+      return response.status(404).json({ error:"Task not found"})
+    }
+    await student.destroy();
+    response.sendStatus(200);
+  } catch(error) {
+    response.status(500).json({error: "Failed to fetch a task"})
+  }
+});
+
+//Create a new student;
+router.post("/", async (response, request) => {
+  try {
+    const student = await Student.create(request.body);
+    response.status(201).send(student);
+  } catch (error) {
+    res.status(500).json({error: "Failed to create a student"})
+  }
+}) 
 
 module.exports = router;
+
+// 1: Axios call to vercel version of my app
+// 2: wait until the axios call completes (await)
+//    a: our request goes to our server 
+//    b: our server queries the database 
+//    c: our database return data to the server 
+//    d: our server answres the request with a response
+// 3: update state of component with the response.data (data= [task1, task2])
