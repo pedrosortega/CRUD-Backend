3d29dedcf0e2b2176cd54a65a244aa6271a81b31
rend1027 <rend1027@yahoo.com>
2025-07-01 22:47:23 -0400

    created GET students/:id

--- api/campuses.js
+++ api/campuses.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const router = express.Router();
-const {Campuses} = require('../database');
+// const express = require('express');
+// const router = express.Router();
+// const {Campuses} = require('../database');
 
-router.get('/', (request, response) => {
-    try{
+// router.get('/', (request, response) => {
+//     try{
         
-    } catch(error) {
-        console.log("Failed to fetch all ")
-        response.send(500).json({error: ""})
-    }
-})
+//     } catch(error) {
+//         console.log("Failed to fetch all ")
+//         response.send(500).json({error: ""})
+//     }
+// })
--- api/students.js
+++ api/students.js
@@ -12,4 +12,17 @@ router.get("/", async (req, res) => {
   }
 });
 
+//Get students by id
+router.get("/:id", async (req, res) => {
+  try {
+    const student = await Student.findByPk(req.params.id);
+    if (!student) {
+      return res.status(404).json({error: "Student not find all"});
+    }
+    res.send(student);
+  } catch(error) {
+    res.status(500).json({ error: "Failed to fetch task"});
+  }
+})
+
 module.exports = router;
