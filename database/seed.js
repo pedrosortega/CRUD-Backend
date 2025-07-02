35cca4aa84791c518057cb5a85e9e6d8991eb05b
Dagostocsc <Deborahagosto19@gmail.com>
2025-07-02 00:12:25 -0400

    Added the Campus Model

--- database/seed.js
+++ database/seed.js
@@ -1,72 +1,28 @@
 const db = require("./db");
 const { DataTypes } = require("sequelize");
 
-const Student = db.define('student',
-  {
-    user_id:
-    {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    firstName:
-    {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: { notEmpty: true}
-    },
-    lastName:
-    {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {notEmpty: true }
-    },
-    imageURL:
-    {
-      type: DataTypes.STRING,
-      defaultValue: "https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg",
-      allowNull: true
-    },
-    email: 
-    {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {notEmpty: true}
-    },
-    gpa:
-    {
-      type: DataTypes.FLOAT(0.0),
-      allowNull: false,
-    } 
-  } 
-)
   const seed = async () => {
   db.logging = false;
-  await db.sync({ force: true }); // Drop and recreate tables
+  await db.sync({ force: true }); 
   const students = await Student.bulkCreate([
     { firstName: "Florencio", lastName: "Rendon", email: "flo@gmail.com", gpa: 3.9},
     { firstName: "Deb", lastName: "Agosto", email: "deb@gmail.com", gpa: 3.9 },
     { firstName: "Pedro", lastName: "Ortega", email: "pedro@gmail.com", gpa: 3.9 },
     { firstName: "Mark", lastName: "Fa", email: "mark@gmail.com", gpa: 3.9}
   ]);
-
+  
+  const campus = await Campus.bulkCreate([
+    {name: "BMCC", address: "245 Greenwich St New York, NY", description: "Start here, go anywhere"}
+  ]);
+  console.log("🌱 Seeded the database again");
+  db.close();
+}
   console.log("🌱 Seeded the database");
   db.close();
-};
+
+  const BMCC = async () => {
+  db.logging = false;
+  await db.sync({ force: true }); 
+  }
 
 seed();
-
-// Table students {
-//   id INTEGER [primary key]
-//   firstName STRING
-//   lastName STRING
-//   email STRING
-//   imageUrl STRING
-//   gpa FLOAT
-// }
-
-// Table campuses {
-//   id INTEGER [primary key]
-//   name STRING
-// }
