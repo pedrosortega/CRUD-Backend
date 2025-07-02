2e6f2764bdc54facf0c3e0d1671db244c4c3dde2
rend1027 <rend1027@yahoo.com>
2025-07-01 23:03:22 -0400

    created patch by id

--- api/students.js
+++ api/students.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const router = express.Router();
+const router = express.Router();   // // creates a modular router instance ---reusable
 const Student  = require("../database/seed");
+const { request } = require("../app");
 
 // GET all students
 router.get("/", async (req, res) => {
@@ -17,11 +18,25 @@ router.get("/:id", async (req, res) => {
   try {
     const student = await Student.findByPk(req.params.id);
     if (!student) {
-      return res.status(404).json({error: "Student not find all"});
+      return res.status(404).json({error: "Student not found"});
     }
     res.send(student);
   } catch(error) {
     res.status(500).json({ error: "Failed to fetch task"});
+  }
+});
+
+//Patch a task by id
+router.patch("/:id", async (request, response) => {
+  try {
+    const student = await Student.findByPk(req.params.id);
+    if (!student) {
+      return res.status(404).json({error: "Student not found"})
+    }
+    const updatedStudent = await student.update(req.body);
+    res.send(updatedStudent);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch task"})
   }
 })
 
